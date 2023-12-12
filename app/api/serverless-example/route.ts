bb5c2b602b576661cb36fb7944b579769e4458ba
Annie Sexton <anniesexton@Annies-Laptop.local>
2023-12-11 18:47:59 -0800

    WIP

--- app/api/serverless-example/route.ts
+++ app/api/serverless-example/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+
 export function GET(request: NextRequest) {
+  console.log("====== Hit the `serverless-example` route");
   return NextResponse.json(
     {
       hello: "butts"
